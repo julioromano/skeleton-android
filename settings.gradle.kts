dbdd9b5385eaf96995e35191fb358067f3d9c216
Marco Romano <marcojulioromano@gmail.com>
2023-03-08 13:48:19 +0100

    Make feature module resources private (#16)
    - Make feature module resources private
    - Reformat .kts files to use 2 spaces per tab

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,59 +3,59 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "skeleton"
 
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("android.arch.*")
-                includeGroupByRegex("androidx.*")
-                includeGroupByRegex("com.android.*")
-                includeGroupByRegex("com.google.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("android.arch.*")
+        includeGroupByRegex("androidx.*")
+        includeGroupByRegex("com.android.*")
+        includeGroupByRegex("com.google.*")
+      }
     }
-    includeBuild("build-logic")
+    mavenCentral()
+    gradlePluginPortal()
+  }
+  includeBuild("build-logic")
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("android.arch.*")
-                includeGroupByRegex("androidx.*")
-                includeGroupByRegex("com.android.*")
-                includeGroupByRegex("com.google.*")
-            }
-        }
-        mavenCentral()
-
-        // Enable only if jitpack is required.
-        //
-        // maven("https://jitpack.io") {
-        //     content {
-        //         includeGroupByRegex("com.github.*")
-        //     }
-        // }
-
-        // For compose compiler build snapshots.
-        maven("https://androidx.dev/storage/compose-compiler/repository/") {
-            content {
-                includeGroup("androidx.compose.compiler")
-            }
-        }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("android.arch.*")
+        includeGroupByRegex("androidx.*")
+        includeGroupByRegex("com.android.*")
+        includeGroupByRegex("com.google.*")
+      }
     }
+    mavenCentral()
+
+    // Enable only if jitpack is required.
+    //
+    // maven("https://jitpack.io") {
+    //     content {
+    //         includeGroupByRegex("com.github.*")
+    //     }
+    // }
+
+    // For compose compiler build snapshots.
+    maven("https://androidx.dev/storage/compose-compiler/repository/") {
+      content {
+        includeGroup("androidx.compose.compiler")
+      }
+    }
+  }
 }
 
 plugins {
-    `gradle-enterprise`
-    id("conventions")
+  `gradle-enterprise`
+  id("conventions")
 }
 
 gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
 }
