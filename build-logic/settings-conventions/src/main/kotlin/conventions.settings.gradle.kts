81ee8bc9c90ead70f3e02d8ff616cfe04aca8aa1
Muhamad Wahyudin <muhammad.whydn@gmail.com>
2024-02-09 13:17:32 +0700

    fix: Dynamically adjust path separator in for OS compatibility
     This commit adjust platform-specific handling of path separators in `conventions.settings.gradle.kts`, to avoid gradle build failure in specific operating system.

--- build-logic/settings-conventions/src/main/kotlin/conventions.settings.gradle.kts
+++ build-logic/settings-conventions/src/main/kotlin/conventions.settings.gradle.kts
@@ -1,18 +1,22 @@
+import org.gradle.internal.os.OperatingSystem
+
+val separator = if (OperatingSystem.current().isWindows) '\\' else '/'
+
 /**
  * Searches for subprojects' `build.gradle` files and prints their Gradle project path.
  */
 private val discoverSubprojects: () -> Array<String> = {
   rootDir.walkBottomUp().filter {
     it.parent != rootDir.path &&
-      it.parent != rootDir.path + "/buildSrc" &&
-      it.parent != rootDir.path + "/build-logic" &&
-      it.parent != rootDir.path + "/build-logic/build-conventions" &&
-      it.parent != rootDir.path + "/build-logic/settings-conventions" &&
+      it.parent != rootDir.path + "${separator}buildSrc" &&
+      it.parent != rootDir.path + "${separator}build-logic" &&
+      it.parent != rootDir.path + "${separator}build-logic${separator}build-conventions" &&
+      it.parent != rootDir.path + "${separator}build-logic${separator}settings-conventions" &&
       it.name == "build.gradle.kts"
   }.map {
     it.path.removePrefix(rootDir.path)
-      .removeSuffix("/build.gradle.kts")
-      .replace('/', ':')
+      .removeSuffix("${separator}build.gradle.kts")
+      .replace(separator, ':')
   }.toList().toTypedArray()
 }
 
